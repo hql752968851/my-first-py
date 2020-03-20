84b1946a1049710209aaf91ed72bb81a5c58b6b8
hql752968851 <2521253602@qq.com>
2020-03-20 15:12:54 +0800

    3.20

--- 1.py
+++ 1.py
@@ -1,5 +1,13 @@
 import    requests
-url="http://www.bilibili.com"
-response=requests .get(url)
-print(response.text)
+from bs4   import BeautifulSoup
+url="http://www.cntour.cn/"
+response=requests.get(url)
+#print(response.text)
+soup=BeautifulSoup(response.text,'lxml')
+data=soup.select("#main > article > div.kratos-hentry.kratos-post-inner.clearfix")
+print(data)
+for item in data;
+    result={
+    'title':item
+}
 
---
+++ post.py
@@ -0,0 +1,19 @@
+import requests
+url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+form_data={
+'i':'我和你都是中国人',
+'from':'AUTO',
+'to':'AUTO',
+'smartresult':'dict',
+'client':'fanyideskweb',
+'salt':'15846868788059',
+'sign':'f72f881076baa0bd359c8ed705df4000',
+'ts':'1584686878805',
+'bv':'fde1c7f2fa80553b128e6ed1e20e3b1b',
+'doctype':'json',
+'version':'2.1',
+'keyfrom':'fanyi.web',
+'action':'FY_BY_REALTlME',
+}
+response=requests.post(url,form_data)
+print(response.text)
